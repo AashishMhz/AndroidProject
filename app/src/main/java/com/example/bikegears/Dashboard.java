8d06b0d29b307c9036160a366fa20370d2e97912
Aashish Maharjan <aashishmaharjan10@gmail.com>
2019-05-29 18:53:27 +0545

    Login and Register Fragment Added

---
+++ app/src/main/java/com/example/bikegears/Dashboard.java
@@ -0,0 +1,5 @@
+package com.example.bikegears;
+
+public class Dashboard {
+
+}
--- app/src/main/java/com/example/bikegears/MainActivity.java
+++ app/src/main/java/com/example/bikegears/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.bikegears.model;
+package com.example.bikegears;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
