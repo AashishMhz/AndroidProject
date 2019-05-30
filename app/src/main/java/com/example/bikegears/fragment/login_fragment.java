6fc72abe4cf2c8ba57d6d961cece943c191ad328
Aashish Maharjan <aashishmaharjan10@gmail.com>
2019-05-30 22:14:10 +0545

    Modify Login Fragment

--- app/src/main/java/com/example/bikegears/fragment/login_fragment.java
+++ app/src/main/java/com/example/bikegears/fragment/login_fragment.java
@@ -1,9 +1,16 @@
 package com.example.bikegears.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.bikegears.R;
 
 public class login_fragment extends Fragment implements View.OnClickListener {
     private EditText login_username, login_password;
@@ -14,5 +21,17 @@ public class login_fragment extends Fragment implements View.OnClickListener {
 
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.activity_login, container, false);
+        login_btn=view.findViewById(R.id.login_btn);
+        login_username=view.findViewById(R.id.login_username);
+        login_password=view.findViewById(R.id.login_password);
 
+        login_btn.setOnClickListener(this);
+
+        return view;
+    }
 }
